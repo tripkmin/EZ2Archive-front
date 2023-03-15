efb64f3ec88204743fed1fa324e8f253fb29dba5
이근민 <tripkmin@gmail.com>
2023-03-16 04:11:42 +0900

    pleaseSelectAchievement 이미지 수정

--- src/routes/guider/PleaseSelectAchievement.jsx
+++ src/routes/guider/PleaseSelectAchievement.jsx
@@ -3,7 +3,7 @@
 const PleaseSelectAchievement = () => {
   return (
     <div className="pleaseSelectBox">
-      <img src={process.env.PUBLIC_URL + '/source/pleaseSelectAchievement.png'} alt="PleaseSelectAchievement"></img>
+      <img src={process.env.PUBLIC_URL + '/source/pleaseSelectA.png'} alt="PleaseSelectAchievement"></img>
       <h3>키와 난이도를 선택해주세요!</h3>
       <h5>부제목</h5>
     </div>
